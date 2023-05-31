3cf4648c3b01c41350ef0b514e499c35f159fbb0
Sandesh Shakya <sandeshakya94@gmail.com>
2023-05-31 00:46:34 -0600

    added initial dependencies

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,5 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) =>
+{
+    return Results.Ok(zip);
+});
 
 app.Run();
