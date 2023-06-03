8ad78220caf58b96389f879001fe8fdf8cd36c15
Sandesh Shakya <sandeshakya94@gmail.com>
2023-06-02 22:50:47 -0600

    added data access to precipitation service

---
+++ CloudWeather.Precipitation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Precipitation.DataAccess
+{
+	public class PrecipDbContext: DbContext
+	{
+		public PrecipDbContext() { }
+		public PrecipDbContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Precipitation> Precipitation { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SnakeCaseTableName(modelBuilder);
+        }
+
+        private static void SnakeCaseTableName(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Precipitation>(b => b.ToTable("precipitation"));
+        }
+    }
+}
+
--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CloudWeather.Precipitation.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
+builder.Services.AddDbContext<PrecipDbContext>(
+    options =>
+    {
+        options.EnableSensitiveDataLogging();
+        options.EnableDetailedErrors();
+        options.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+
+    }, ServiceLifetime.Transient);
+
 var app = builder.Build();
 
-app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) =>
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) =>
 {
-    return Results.Ok(zip);
+    if (days == null || days < 1 || days > 30) {
+        return Results.BadRequest("Enter a 'days' parameter between 1 and 30");
+    }
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.Precipitation
+        .Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate)
+        .ToListAsync();
+    return Results.Ok(results);
 });
+
 
 app.Run();
